da0096723fd324511f477225e57d1d56124da69f
Nilanjana Dey <128115263+Nilanjana30@users.noreply.github.com>
2024-06-30 12:09:42 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import styles from "./App.module.css";
-import { About } from "./components/About/About";
-import { Contact } from "./components/Contact/Contact";
-import { Experience } from "./components/Experience/Experience";
-import { Hero } from "./components/Hero/Hero";
-import { Navbar } from "./components/Navbar/Navbar";
-import { Projects } from "./components/Projects/Projects";
-
+import index from index;
 function App() {
   return (
-    <div className={styles.App}>
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-    </div>
+    <index/>
+    
+   
   );
 }
 
---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
+function index() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+export default index;
