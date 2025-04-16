57b597eb1bb25ef556646e7aa26fd5ef56ae9be2
Nilanjana Dey <128115263+Nilanjana30@users.noreply.github.com>
2025-04-16 21:30:40 +0530

    app.js fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import Index from "./Index.jsx";
+import React from "react";
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
-  return <Index />;
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
